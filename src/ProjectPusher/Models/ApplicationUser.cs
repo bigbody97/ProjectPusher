9dbaee845c88af7d0ddbcf7f188f47e64dd62a71
bigbody97 <famback17@gmail.com>
2017-04-15 17:20:51 -0700

    I updated the appUser with new props

--- src/ProjectPusher/Models/ApplicationUser.cs
+++ src/ProjectPusher/Models/ApplicationUser.cs
@@ -9,5 +9,21 @@ namespace ProjectPusher.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastNamr { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Skill { get; set; }
+        public string ProfileImag { get; set; }
+        public int BirthDay { get; set; }
+        public string BirthMonth { get; set; }
+        public int BirthYear { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsOpenToBarter { get; set; }
+        public string Bio { get; set; }
+        public string ForSale { get; set; }
+        public string LookingFor { get; set; }
+        public string StatusMessage { get; set; }
     }
 }
